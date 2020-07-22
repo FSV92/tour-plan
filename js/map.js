7eb12752dacf9a8714a895a44af4797b0dc33dbd
Sergey Filonov <filonov_sv@mail.ru>
2020-07-23 00:33:35 +0800

    fixed: corrected

--- js/map.js
+++ js/map.js
@@ -5,4 +5,9 @@ function init() {
         center: [7.838196, 98.298813],
         zoom: 15
     });
-};
+    var myPlacemark = new ymaps.Placemark(
+        [7.838196, 98.298813]
+    );
+
+    myMap.geoObjects.add(myPlacemark);
+}
